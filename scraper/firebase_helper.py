6b19880bd7c8d236fc7482ea5e20b69aa5ab2db9
rahulbkn <50168817+rahulbkn@users.noreply.github.com>
2025-09-02 22:03:22 +0530

    Update firebase_helper.py

--- scraper/firebase_helper.py
+++ scraper/firebase_helper.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, db
 # Initialize Firebase (replace with your key)
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://YOUR_PROJECT_ID.firebaseio.com/'
+    'databaseURL': 'https://wallmob-wallpaper-default-rtdb.asia-southeast1.firebasedatabase.app/'
 })
 
 def push_to_firebase(urls, node="wallpapers"):
